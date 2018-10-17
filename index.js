ef94424541431767a34d0bec6b24473fdad84266
kasongoyo <kasongoyo@gmail.com>
2018-10-17 21:26:27 +0300

    Prefix error with the keyword custom

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ class DomainError extends Error {
   constructor(error) {
     super();
     // Ensure the name of this error is the same as the class name
-    this.name = this.constructor.name;
+    this.name = `Custom${this.constructor.name}`;
     if (typeof error === 'string') {
       // The argument passed is of string type so it means that's error message
       this.message = error;
